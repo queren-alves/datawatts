07dab2c1cecbf39d35c75c48257f38081bbc487f
Queren <querenhalves@outlook.com>
2025-09-10 16:44:15 -0300

    página de login

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.classList.add("solid");
-  } else {
-    header.classList.remove("solid");
-  }
-});
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+      header.classList.add("solid");
+    } else {
+      header.classList.remove("solid");
+    }
+  });
 
   const menuToggle = document.getElementById("menu-toggle");
   const navMenu = document.getElementById("nav-menu");
@@ -20,3 +20,38 @@ window.addEventListener("scroll", function () {
   });
 
 });
+
+const tabs = document.querySelectorAll('.tab');
+const forms = document.querySelectorAll('form');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    forms.forEach(f => f.classList.remove('active'));
+
+    tab.classList.add('active');
+    document.getElementById('form-' + tab.dataset.tab).classList.add('active');
+  });
+});
+
+const inputs = document.querySelectorAll("input");
+
+  inputs.forEach(input => {
+    const errorMsg = input.nextElementSibling;
+
+    input.addEventListener("input", () => {
+      if (!input.checkValidity()) {
+        errorMsg.style.display = "block";
+      } else {
+        errorMsg.style.display = "none";
+      }
+    });
+
+    input.addEventListener("blur", () => {
+      if (!input.checkValidity()) {
+        errorMsg.style.display = "block";
+      } else {
+        errorMsg.style.display = "none";
+      }
+    });
+  });
